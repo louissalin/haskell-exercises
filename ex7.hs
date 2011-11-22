bc9ef5093499abbd7cd6fc7166d57e0449dd1161
Louis Salin <louis.phil@gmail.com>
2011-11-22 11:31:49 -0600

    finished the simple exercises

--- ex7.hs
+++ ex7.hs
@@ -16,7 +16,7 @@ myMap f = foldr (\x y -> (f x):y) []
 
 dec2int = foldl (\n x -> n*10 + x) 0
 {- [x1,x2,x3,x4] -> (((0*10 + x1)*10 + x2)*10 + x3)*10 + x4 -}
-{-wow, this works well and I never thought of it this way. I had too google the answer-}
+{-wow, this works well and I never thought of it this way. I had to google the answer-}
 
 {-compose [...] exercise:-}
 {-because all element of a list must have the same time.-}
@@ -27,3 +27,29 @@ dec2int = foldl (\n x -> n*10 + x) 0
 {-the other two elements are functions that take lists and return lists.-}
 
 {-so the first element is of a different type than the other two and the list is illegal.-}
+
+myCurry :: ((a, b) -> c) -> a -> b -> c
+myCurry f = \a b -> f (a, b)
+
+myUncurry :: (a -> b -> c) -> (a, b) -> c
+myUncurry f = \p -> f (fst p)  (snd p)
+
+{-prelude definition of curry and uncurry:-}
+{-curry f x y = f (x, y)-}
+{-uncurry f p = f (fst p) (snd p)-}
+
+{-I wonder if returning lambda is good or not. I didn't have to specify the parameters in my definition...-}
+
+unfold p h t x | p x 	   = []
+			   | otherwise = h x : unfold p h t (t x)
+
+int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+
+chop8 :: [Int] -> [[Int]]
+chop8 = unfold null (take 8) (drop 8)
+
+myMap2 :: (a -> b) -> [a] -> [b]
+myMap2 f = unfold null (f . head) tail
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f = unfold (\_ -> False) id f
